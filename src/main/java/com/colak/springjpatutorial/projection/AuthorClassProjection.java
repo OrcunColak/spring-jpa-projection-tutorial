6594a29afee7c39a24f36ffdd1aa733abaf870b6
Orçun Çolak <orcun.colak@hazelcast.com>
2024-07-24 09:41:05 +0300

    spring update

--- src/main/java/com/colak/springjpatutorial/projection/AuthorClassProjection.java
+++ src/main/java/com/colak/springjpatutorial/projection/AuthorClassProjection.java
@@ -2,8 +2,9 @@ package com.colak.springjpatutorial.projection;
 
 import java.io.Serializable;
 
-/**
- * Example for class/record based projection
- */
+
+// Example for class/record based projection
+// See https://stackoverflow.com/questions/78784503/how-to-cache-the-view-result-of-a-spring-jpa-query
+// to see why record is Serializable
 public record AuthorClassProjection(Long id, String name) implements Serializable {
 }
