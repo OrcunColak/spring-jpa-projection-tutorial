2249dab0547e6030ad3fd75f8a898be51ca410e3
Orçun Çolak <orcun.colak@hazelcast.com>
2024-07-24 09:39:45 +0300

    spring update

--- src/main/java/com/colak/springjpatutorial/projection/AuthorClassProjection.java
+++ src/main/java/com/colak/springjpatutorial/projection/AuthorClassProjection.java
@@ -1,7 +1,9 @@
 package com.colak.springjpatutorial.projection;
 
+import java.io.Serializable;
+
 /**
  * Example for class/record based projection
  */
-public record AuthorClassProjection(Long id, String name) {
+public record AuthorClassProjection(Long id, String name) implements Serializable {
 }
